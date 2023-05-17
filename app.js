958e1fec9cade3c98fbcb52ce21d7b8c0f459450
Shashi Kumar S <shashi@Shashis-MacBook-Air.local>
2023-05-17 23:21:25 +0530

    url chnages for googe auth

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ passport.serializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://secrets-nkoc.onrender.com/auth/google/secrets",
   },
   
   function(accessToken, refreshToken, profile, cb) {
