7ffeb63313ee2210bafd8d80529f0dc99bf32337
Shashi Kumar S <shashi@Shashis-MacBook-Air.local>
2023-05-20 14:45:58 +0530

    submit secret bug fix

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
     secret: 'Our little secret.',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
+    // cookie: { secure: true }
   }));
 
   app.use(passport.initialize());
@@ -79,7 +79,8 @@ passport.serializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://secrets-thv7.onrender.com/auth/google/secrets"
+    callbackURL: "https://secrets-thv7.onrender.com/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   
   function(accessToken, refreshToken, profile, cb) {
